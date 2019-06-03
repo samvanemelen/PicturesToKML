86e077eac4e7a8bb38858e0a45fbad76f7cddbf0
samvanemelen <samvanemelen@users.noreply.github.com>
2019-06-03 14:48:31 +0200

    Uploading files

---
+++ PicturesToKML.py
@@ -0,0 +1,95 @@
+import os
+from PIL import Image
+from PIL.ExifTags import TAGS, GPSTAGS
+
+print('Loading requirements...')
+
+def get_exif(filename):
+    # Extract the Exif Data from an image using PIL.Image
+    image = Image.open(filename)
+    image.verify()
+    return image._getexif()
+
+def get_geotagging(exif):
+    if not exif:
+        raise ValueError("No EXIF metadata found")
+
+    geotagging = {}
+    for (idx, tag) in TAGS.items():
+        if tag == 'GPSInfo':
+            if idx not in exif:
+                return
+
+            for (key, val) in GPSTAGS.items():
+                if key in exif[idx]:
+                    geotagging[val] = exif[idx][key]
+
+    return geotagging
+
+def get_decimal_from_dms(dms, ref):
+
+    degrees = dms[0][0] / dms[0][1]
+    minutes = dms[1][0] / dms[1][1] / 60.0
+    seconds = dms[2][0] / dms[2][1] / 3600.0
+
+    if ref in ['S', 'W']:
+        degrees = -degrees
+        minutes = -minutes
+        seconds = -seconds
+
+    return round(degrees + minutes + seconds, 5)
+
+def get_coordinates(geotags):
+    lat = get_decimal_from_dms(geotags['GPSLatitude'], geotags['GPSLatitudeRef'])
+    lon = get_decimal_from_dms(geotags['GPSLongitude'], geotags['GPSLongitudeRef'])
+
+    return (lat,lon)
+
+Title = input('What should the name of the KML be? ')
+
+KML = '<?xml version="1.0" encoding="UTF-8"?>\n'
+KML += '<kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2" xmlns:kml="http://www.opengis.net/kml/2.2" xmlns:atom="http://www.w3.org/2005/Atom">\n'
+KML += '<Document>\n<name>output.kml</name>\n'
+KML += '<Style id="s_ylw-pushpin">\n'
+KML += '<IconStyle>\n<scale>1.1</scale><Icon><href>http://maps.google.com/mapfiles/kml/paddle/red-circle.png</href>'
+KML += '</Icon><hotSpot x="20" y="2" xunits="pixels" yunits="pixels"/></IconStyle></Style>\n'
+KML += '<StyleMap id="m_ylw-pushpin"><Pair><key>normal</key><styleUrl>s_ylw-pushpin</styleUrl></Pair></StyleMap>\n'
+KML += '<name>'+Title+'</name>\n<open>1</open>\n'
+
+i = 0
+n = 0
+path = os.path.dirname(os.path.realpath(__file__))
+for filename in os.listdir(path):
+    try:
+        if filename.endswith(".jpg") or filename.endswith(".jpeg"):
+            n += 1
+            exif = get_exif(filename)
+            geotagging = get_geotagging(exif)
+            if geotagging != None:
+                i += 1
+                print('Placing image #'+str(i))
+                coordinates = get_coordinates(geotagging)
+                KML += '<Placemark>\n<name>Picture ' + str(i) + '</name>\n'
+                KML += '<description>'
+                KML += '<p>Taken with a '+exif[272].rstrip('\x00')+'</p>'
+                try:
+                    KML += '<p>At: '+exif[306]+'</p>'
+                except:
+                    pass
+                KML += '<![CDATA[<img style="max-width:500px;" src="file:///'+path + '\\' + filename+'">]]></description>\n'
+                KML += '<LookAt>\n<longitude>'+str(coordinates[1])+'</longitude>\n'
+                KML += '<latitude>'+str(coordinates[0])+'</latitude>\n</LookAt>\n'
+                KML += '<styleUrl>m_ylw-pushpin</styleUrl>\n'
+                KML += '<Point>\n<gx:drawOrder>1</gx:drawOrder>\n'
+                KML += '<coordinates>'+str(coordinates[1])+','+str(coordinates[0])+'</coordinates>\n'
+                KML += '</Point>\n</Placemark>\n'
+    except:
+        pass
+
+KML += '</Document></kml>\n'
+
+print('Saving file')
+with open("Output.kml", "w") as text_file:
+    text_file.write(KML)
+print(str(i) + ' of the ' + str(n) + ' images had a location in their metadata.')
+input('Successfully placed these images on the map. Press enter to exit')
--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# PicturesToKML
+# Pictures to KML
+
+Whether you have an album from a vacation, a field work, a study or any other activity, displaying those images in a spatial context such as Google Earth can be very useful. 
+
+Pictures to KML is a tool that can be placed in a folder and extracts all geo-tagged information from images in that folder. It creates a KML file that can be read by a variety of tools, most commonly Google Earth. The KML Placemarks contain the image itself, the device and the date.
+
+## Files
+
+This repository contains 2 files. The python file constructed to do the operations, and an .exe file. This .exe file is an executable from this python script created with PyInstaller. Just download the .exe file and place it in the folder containing geo-tagged images. Run the file and a KML file will be created.
+The python file works in the same way but requires the right modules to be present in your environment. The executable file does not has dependencies.
+
+## Updates and contributions
+
+Feel free to contribute with issues, remarks, improvements of forking this repository. It is not actively being developed but occurring issues or proposed improvements might be incorporated anyway.
+
+## Users guide
+![ExeInFolder](https://user-images.githubusercontent.com/36103001/58802011-5f69a780-860c-11e9-9791-34a18d22c3e8.JPG)
+First of all, place the executable (or the python script) in the folder containing your images.
+![Give a name window](https://user-images.githubusercontent.com/36103001/58802012-5f69a780-860c-11e9-91a0-02a037c28b61.JPG)
+When opening the exe file (or running the python script) You are prompted to give a name. This is not the name of the file (Always Output.kml) but the name of the feature group.
+
+![Placing images](https://user-images.githubusercontent.com/36103001/58802009-5ed11100-860c-11e9-9270-d6eb7807bad5.JPG)
+after that the code will check all images with geodata and adds those with a location to the KML.
+
+![Done](https://user-images.githubusercontent.com/36103001/58802010-5f69a780-860c-11e9-84b1-e3474e87928f.JPG)
+At the end you will see a confirmation and a line telling you how many images were added to the KML file.
+
+
+![GoogleEarth](https://user-images.githubusercontent.com/36103001/58802013-5f69a780-860c-11e9-88cf-5307bf92c2c3.JPG)
+The final result looks something like this. A map with a placemark for each image, with a short description and the image itself.
